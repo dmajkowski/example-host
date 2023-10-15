61e0c87dcb13ce9d9ca0414f888b33bf4f448745
Dariusz.Majkowski@reckitt.com <Dariusz.Majkowski@reckitt.com>
2023-10-15 09:19:41 +0200

    Microfrontend with two remotes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
+const Header = lazy(() => import('ExampleRemote/Header'));
+const Footer = lazy(() => import('ExampleRemote/Footer'));
+const Clicker = lazy(() => import('ExampleRemote/Clicker'));
+const Content = lazy(() => import('ExampleRemoteSite/Content'));
 
 function App() {
+  const [hostState, setHostState] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Ładowanie Headera...</div>}>
+        <Header />
+      </Suspense>
+      <p>
+        Host App - {hostState}
+      </p>
+      <Suspense fallback={<div>Ładowanie Clickera...</div>}>
+        <Clicker hostState={hostState} setHostState={setHostState}/>
+      </Suspense>
+      <Suspense fallback={<div>Ładowanie Clickera...</div>}>
+        <Content/>
+      </Suspense>
+      <Suspense fallback={<div>Ładowanie Footera...</div>}>
+        <Footer />
+      </Suspense>
+      
     </div>
   );
 }
